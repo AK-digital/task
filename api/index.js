3ef9b120916ca25f0db98e69a3fb95ae54ba5061
= <=>
2024-12-23 16:33:38 +0100

    first commit

---
+++ api/index.js
@@ -0,0 +1,29 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import "./config/db.js";
+import passport from "passport";
+import authRouter from "./routes/auth.routes.js";
+import userRouter from "./routes/user.routes.js";
+import projectRouter from "./routes/project.routes.js";
+import boardRouter from "./routes/board.routes.js";
+
+const app = express();
+app.use(express.json());
+
+app.use(passport.initialize());
+
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
+
+app.use("/api/project", projectRouter);
+app.use("/api/board", boardRouter);
+
+app.listen(process.env.PORT || 4000, (err) => {
+  console.log(process.env.PORT);
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`server is listening on port ${process.env.PORT}`);
+  }
+});
