bbb4717f0c69c7fa7f691f8ccd8ad22ddaa484ae
Aurélien Kohler <aurelien@akdigital.fr>
2025-02-05 16:12:53 +0100

    update emails.js

--- api/templates/emails.js
+++ api/templates/emails.js
@@ -2,10 +2,9 @@ export function emailDescription(user, task) {
   const templates = {
     subjet: `Vous avez été identifié dans une description`,
     text: `
-      <div style="font-family: Arial, sans-serif; color: #5C5D61;; text-align: center; padding: 20px; margin-inline: auto;">
+      <div style="font-family: Arial, sans-serif; color: #5C5D61; text-align: center; padding: 20px; margin-inline: auto;">
       <h1 style="font-size: 40px; font-weight: bold;">Hey ! Par ici 🙋‍♂️</h1>
-      <h2 style="font-size: 28px; max-width: 450px; margin-inline: auto; font-weight: 500;">${
-        user?.firstName + " " + user?.lastName
+      <h2 style="font-size: 28px; max-width: 450px; margin-inline: auto; font-weight: 500;">${user?.firstName + " " + user?.lastName
       } vous a mentionné dans <span style="color: #777AE4;">cette description</span>
       </h2>
       <div style="font-size: 20px; font-weight: 300;">
@@ -27,10 +26,9 @@ export function emailMessage(user, message) {
   const templates = {
     subjet: `Vous avez été identifié dans une conversation`,
     text: `
-      <div style="font-family: Arial, sans-serif; color: #5C5D61;; text-align: center; padding: 20px; margin-inline: auto;">
+      <div style="font-family: Arial, sans-serif; color: #5C5D61; text-align: center; padding: 20px; margin-inline: auto;">
       <h1 style="font-size: 40px; font-weight: bold;">Hey ! Par ici 🙋‍♂️</h1>
-      <h2 style="font-size: 28px; max-width: 450px; margin-inline: auto; font-weight: 500;">${
-        user?.firstName + " " + user?.lastName
+      <h2 style="font-size: 28px; max-width: 450px; margin-inline: auto; font-weight: 500;">${user?.firstName + " " + user?.lastName
       } vous a mentionné dans <span style="color: #777AE4;">cette conversation</span>
       </h2>
       <div style="font-size: 20px; font-weight: 300;">
@@ -52,9 +50,9 @@ export function emailProjectInvitation(project, sender, link) {
   const templates = {
     subjet: `Vous avez été invité à rejoindre un projet`,
     text: `
-      <div style="font-family: Arial, sans-serif; color: #5C5D61;; text-align: center; padding: 20px; margin-inline: auto;">
+      <div style="font-family: Arial, sans-serif; color: #5C5D61; text-align: center; padding: 20px; margin-inline: auto;">
       <h1 style="font-size: 40px; font-weight: bold;">La chaaance 🍀</h1>
-      <div style="font-size: 20px; font-weight: 300; max-width: 300px;">
+      <div style="font-size: 20px; font-weight: 300; max-width: 300px;text-align: center;">
        <p>
          ${sender?.firstName + " " + sender?.lastName}
           vous a invité à rejoindre le projet <span>${project?.name}</span>
