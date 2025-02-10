35415674745acb29e4e4808334396f56d44810b1
Hyalin <nicolas.tombal01@gmail.com>
2025-02-10 09:22:50 +0100

    Possible fix for responsible

--- api/index.js
+++ api/index.js
@@ -20,6 +20,8 @@ const app = express();
 const server = createServer(app);
 
 const io = new Server(server, {
+  addTrailingSlash: false,
+  transports: ["websocket", "polling"],
   cors: {
     origin: process.env.CLIENT_URL,
     methods: ["GET", "POST", "PUT", "DELETE"],
--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -1,6 +1,14 @@
 "use client";
 
 import { io } from "socket.io-client";
-const socket = io(`${process.env.NEXT_PUBLIC_SOCKET_API_URL}`);
+
+const socket = io(`${process.env.NEXT_PUBLIC_SOCKET_API_URL}`, {
+  transports: ["websocket", "polling"], // Force l'utilisation de WebSocket d'abord
+  withCredentials: true, // Si votre API nécessite des cookies
+  reconnection: true, // Essaye de se reconnecter automatiquement
+  reconnectionAttempts: 5, // Nombre de tentatives avant d'abandonner
+  reconnectionDelay: 2000, // Temps entre chaque tentative (2s)
+  addTrailingSlash: false,
+});
 
 export default socket;
