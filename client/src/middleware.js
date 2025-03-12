6a98621ac9940c32ef25b10667c2ddf0e46046fc
Hyalin <nicolas.tombal01@gmail.com>
2025-03-12 16:59:34 +0100

    Login page now redirect the user to the projects page if he is already logged in

--- client/src/middleware.js
+++ client/src/middleware.js
@@ -1,11 +1,26 @@
 import { NextResponse } from "next/server";
 import { handleAuth } from "./middlewares/authMiddleware";
+import { getAccessToken } from "./utils/getCookies";
 
 // Le middleware gère l'authentification et l'autorisation
 export async function middleware(request) {
-  return await handleAuth(request, NextResponse);
+  if (request.nextUrl.pathname === "/") {
+    const accessToken = await getAccessToken();
+
+    if (accessToken) {
+      return NextResponse.redirect(new URL("/projects", request.url));
+    }
+  } else {
+    return await handleAuth(request, NextResponse);
+  }
 }
 
 export const config = {
-  matcher: ["/projects", "/projects/:path*", "/profile", "/new-project/:path*"], // Routes protégées
+  matcher: [
+    "/",
+    "/projects",
+    "/projects/:path*",
+    "/profile",
+    "/new-project/:path*",
+  ], // Routes protégées
 };
