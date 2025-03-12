c4f0f396c98eb0f27c2cd859c52c7e7a0cc355b1
Aurélien Kohler <aurelien@akdigital.fr>
2025-03-12 22:49:43 +0100

    update Notifications.jsx and notifications.module.css

--- client/src/components/Notifications/Notifications.jsx
+++ client/src/components/Notifications/Notifications.jsx
@@ -36,7 +36,7 @@ export default function Notifications({ setNotifOpen, notifications, mutate }) {
       socket.off("notifications read", handleReadNotifications);
     };
   }, [socket]);
-  
+
   return (
     <>
       <div className={styles.container} id="popover">
@@ -48,55 +48,58 @@ export default function Notifications({ setNotifOpen, notifications, mutate }) {
             </div>
           )}
         </div>
-          {!isNotEmpty(notifications) ? (
-            <div className={styles.empty}>
-              <span>Vous n'avez aucune notification pour le moment</span>
-            </div>
-          ) : (
-            <ul className={styles.notifications}>
-              {notifications?.map((notif, idx) => {
-                const dateFromNow = moment(notif?.createdAt).fromNow();
-                const isLastItem = idx === notifications.length - 1;
-                
-                return (
-                  <li 
-                    className={`${styles.notification} ${!notif.read ? styles.unread : ''}`} 
-                    key={idx}
-                  >
-                    <Link href={`${notif?.link}`}>
-                      <div className={styles.left}>
-                        {notif?.senderId?.picture ? (
-                          <Image
-                            src={notif?.senderId?.picture}
-                            width={30}
-                            height={30}
-                            alt={`Photo de profil de ${notif?.senderId?.firstName}`}
-                            style={{ borderRadius: "50%" }}
-                          />
-                        ) : (
-                          <NoPicture
-                            user={notif?.senderId}
-                            width={"30px"}
-                            height={"30px"}
-                          />
-                        )}
-                        <div className={styles.message}>
-                          <div className={styles.title}>
-                            <span>{notif?.message?.title}</span>
-                          </div>
-                          <div className={styles.content}>
-                            {notif?.message?.content}
-                          </div>
+        {!isNotEmpty(notifications) ? (
+          <div className={styles.empty}>
+            <span>Vous n'avez aucune notification pour le moment</span>
+          </div>
+        ) : (
+          <ul className={styles.notifications}>
+            {notifications?.map((notif, idx) => {
+              const dateFromNow = moment(notif?.createdAt).fromNow();
+              const isLastItem = idx === notifications.length - 1;
+
+              return (
+                <li
+                  className={`${styles.notification} ${!notif.read ? styles.unread : ''}`}
+                  key={idx}
+                >
+                  <Link href={`${notif?.link}`}>
+                    <div className={styles.left}>
+                      {notif?.senderId?.picture ? (
+                        <Image
+                          src={notif?.senderId?.picture}
+                          width={30}
+                          height={30}
+                          alt={`Photo de profil de ${notif?.senderId?.firstName}`}
+                          style={{ borderRadius: "50%" }}
+                        />
+                      ) : (
+                        <NoPicture
+                          user={notif?.senderId}
+                          width={"30px"}
+                          height={"30px"}
+                        />
+                      )}
+                      <div className={styles.message}>
+                        <div className={styles.title}>
+                          <span>{notif?.message?.title}</span>
+                        </div>
+                        <div className={styles.content}>
+                          {notif?.message?.content}
                         </div>
                       </div>
+                    </div>
+                    <div className={styles.right}>
                       <div className={styles.date}>{dateFromNow}</div>
-                    </Link>
-                  </li>
-                );
-              })}
-            </ul>
-          )}
-        </div>
+                      <div className={styles.bullet}></div>
+                    </div>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        )}
+      </div>
       <div id="modal-layout-opacity" onClick={(e) => setNotifOpen(false)}></div>
     </>
   );
