13b487dc99d18cb443a9e669b4647f0c7512c795
Nacola04 <92257036+Nacola04@users.noreply.github.com>
2025-05-27 16:05:53 +0200

    fix UsersInfo

--- client/src/components/Popups/UsersInfo.jsx
+++ client/src/components/Popups/UsersInfo.jsx
@@ -1,9 +1,11 @@
 import styles from "@/styles/components/popups/usersInfo.module.css";
 import DisplayPicture from "../User/DisplayPicture";
 
-export default function UsersInfo({ users }) {
+export default function UsersInfo({ users, style }) {
+  const top = style?.top || "35px";
+  const left = style?.left || "4px";
   return (
-    <div className={styles.container}>
+    <div className={styles.container} style={{ top: top, left: left }}>
       {users.map((user) => (
         <div key={user?._id} className={styles.infoUser}>
           <DisplayPicture
