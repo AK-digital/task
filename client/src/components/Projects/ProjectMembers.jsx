a42ab656f8ce471fbacf70aafd3c2a9d8a1c7813
Nacola04 <92257036+Nacola04@users.noreply.github.com>
2025-05-27 15:51:32 +0200

    Create ProjectMembers.jsx

---
+++ client/src/components/Projects/ProjectMembers.jsx
@@ -0,0 +1,99 @@
+"use client";
+import styles from "@/styles/components/projects/projectMembers.module.css";
+import { useRef, useState } from "react";
+import UsersInfo from "../Popups/UsersInfo";
+import Portal from "../Portal/Portal";
+import DisplayPicture from "../User/DisplayPicture";
+
+export default function ProjectMembers({ members }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [style, setStyle] = useState(null);
+
+  const moreRef = useRef(null);
+  const hoverTimeout = useRef(null);
+  const defaultUsers = members.map((member) => member.user);
+  const [users, setUsers] = useState(defaultUsers);
+
+  const clearHoverTimeout = () => {
+    if (hoverTimeout.current) {
+      clearTimeout(hoverTimeout.current);
+      hoverTimeout.current = null;
+    }
+  };
+
+  const handleMoreMouseEnter = () => {
+    clearHoverTimeout();
+    if (moreRef.current) {
+      const rect = moreRef.current.getBoundingClientRect();
+      setStyle({
+        top: rect.top + rect.height + window.scrollY - 16,
+        left: rect.left + window.scrollX,
+      });
+      setUsers(defaultUsers);
+
+      setIsOpen(true);
+    }
+  };
+
+  const handleMouseLeave = () => {
+    clearHoverTimeout();
+    hoverTimeout.current = setTimeout(() => {
+      setIsOpen(false);
+    }, 150);
+  };
+
+  const handlePopupMouseEnter = () => {
+    clearHoverTimeout();
+  };
+
+  const handlePopupMouseLeave = () => {
+    clearHoverTimeout();
+    hoverTimeout.current = setTimeout(() => {
+      setIsOpen(false);
+    }, 150);
+  };
+
+  return (
+    <>
+      <div className={styles.membersWrapper}>
+        {/* Membres du projet (affiche max 4) */}
+        {defaultUsers.slice(0, 4).map((user) => (
+          <div key={user?._id} className={styles.memberWrapper}>
+            <DisplayPicture
+              user={user}
+              style={{
+                width: "30px",
+                height: "30px",
+                borderRadius: "50%",
+              }}
+            />
+          </div>
+        ))}
+
+        {/* Affichage "+n" si plus de 4 membres */}
+        {defaultUsers?.length > 4 && (
+          <div
+            className={styles.memberWrapper}
+            ref={moreRef}
+            onMouseEnter={handleMoreMouseEnter}
+            onMouseLeave={handleMouseLeave}
+          >
+            <div className={styles.moreMembers}>+{defaultUsers.length - 4}</div>
+          </div>
+        )}
+      </div>
+
+      {/* PORTAL pour la popup */}
+      {isOpen && style && (
+        <Portal>
+          <div
+            onMouseEnter={handlePopupMouseEnter}
+            onMouseLeave={handlePopupMouseLeave}
+          >
+            <UsersInfo users={users} style={style} />
+          </div>
+        </Portal>
+      )}
+    </>
+  );
+}
