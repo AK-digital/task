7d8d954d003b3d4379532f4a27e005085642d9be
Timothée Van Den Bosch <timotheevdbosch@gmail.com>
2025-06-02 10:04:11 +0200

    Fix: New Project Card CSS

--- client/src/components/Projects/ProjectCard.jsx
+++ client/src/components/Projects/ProjectCard.jsx
@@ -67,8 +67,8 @@ export default function ProjectCard({
   }
 
   return (
-    <div key={projectId} className="relative w-full max-w-[290px] rounded-tr-2xl rounded-br-2xl rounded-bl-2xl overflow-visible transition-all duration-200 ease-in-out cursor-pointer mt-6 no-underline hover:-translate-y-0.5">
-      <div className={`relative ${isDefaultProject ? '-top-[5px] bg-[#e9e7dd]' : 'top-px left-0 bg-background-secondary-color'} w-40 h-[30px] rounded-tl-2xl rounded-tr-0 rounded-bl-0 rounded-br-0 [clip-path:path('M_0_0_L_128_0_C_144_2_136_24,_160_34_L_0_34_Z')]`}>
+    <div key={projectId} className="relative w-full max-w-[290px] rounded-tr-2xl rounded-br-2xl rounded-bl-2xl overflow-visible transition-all duration-200 ease-in-out cursor-pointer mt-6 no-underline hover:-translate-y-0.5 flex flex-col">
+      <div className={`relative w-40 h-[30px] rounded-tl-2xl rounded-tr-0 rounded-bl-0 rounded-br-0 [clip-path:path('M_0_0_L_128_0_C_144_2_136_24,_160_34_L_0_34_Z')] ${isDefaultProject ? 'bg-[#e9e7dd]' : 'top-px left-0 bg-background-secondary-color'}`}>
 
         <Star
           size={16}
@@ -77,46 +77,48 @@ export default function ProjectCard({
           />
       </div>
 
-      <Link href={href}>
-        <div className={`${isDefaultProject ? 'absolute w-full top-6 bg-[#e9e7dd]' : 'bg-background-secondary-color'} py-[18px] px-[22px] rounded-tr-2xl rounded-bl-2xl rounded-br-2xl rounded-tl-none transition-all duration-200 ease-in-out hover:shadow-[10px_10px_10px_rgba(0,0,0,0.1)] hover:!text-transparent`}>
+      <Link href={href} className="no-underline">
+        <div className={`flex-1 py-[18px] px-[22px] rounded-tr-2xl rounded-bl-2xl min-h-[193px] rounded-br-2xl rounded-tl-none transition-all duration-200 ease-in-out hover:shadow-[10px_10px_10px_rgba(0,0,0,0.1)] hover:!text-transparent flex flex-col justify-between ${isDefaultProject ? 'absolute w-full bg-[#e9e7dd]' : 'bg-background-secondary-color'}`}>
           
-          {isDefaultProject ? (
-            <div className="flex justify-between w-full rounded-2xl">
-              <div className="flex items-center justify-center w-[41px] h-[41px] bg-[#d0cec7] rounded-full">
+          <div>
+            {isDefaultProject ? (
+              <div className="flex justify-between w-full rounded-2xl">
+                <div className="flex items-center justify-center w-[45px] h-[45px] bg-[#d0cec7] rounded-full">
+                  <Image
+                    src="/default-project-logo.svg"
+                    alt="project"
+                    width={22}
+                    height={22}
+                    className="rounded-full w-[22px] h-[22px] max-w-[22px] max-h-[22px]"
+                  />
+                </div>
+
+                <div className="flex w-[30px] h-[30px] max-w-[30px] max-h-[30px] rounded-full bg-[#d0cec7]">
+                  <div className="-ml-2"></div>
+                </div>
+              </div>
+              
+            ) : (
+              <div className="flex justify-between w-full rounded-2xl">
                 <Image
-                  src="/default-project-logo.svg"
+                  src={project?.logo || "/default-project-logo.webp"}
                   alt="project"
-                  width={22}
-                  height={22}
-                  className="rounded-full w-[22px] h-[22px] max-w-[22px] max-h-[22px]"
+                  width={45}
+                  height={45}
+                  className="rounded-full w-[45px] h-[45px] max-w-[45px] max-h-[45px] cursor-pointer"
                 />
+
+                <ProjectMembers members={members} />
               </div>
+            )}
 
-              <div className="flex w-[30px] h-[30px] max-w-[30px] max-h-[30px] rounded-full bg-[#d0cec7]">
-                <div className="-ml-2"></div>
-              </div>
+            <div className="text-xl mt-1.5 text-text-darker-color">
+              <span>{name}</span>
             </div>
-            
-          ) : (
-            <div className="flex justify-between w-full rounded-2xl">
-              <Image
-                src={project?.logo || "/default-project-logo.webp"}
-                alt="project"
-                width={45}
-                height={45}
-                className="rounded-full w-[45px] h-[45px] max-w-[45px] max-h-[45px] cursor-pointer"
-              />
-
-              <ProjectMembers members={members} />
-            </div>
-          )}
-
-          <div className="text-xl mt-1.5 text-text-darker-color">
-            <span>{name}</span>
           </div>
 
           {isDefaultProject ? (
-            <div className="flex flex-col gap-2.5 mt-[30px] text-text-size-small">
+            <div className="flex flex-col mt-6 gap-2.5 text-text-size-small flex-1">
               <div className={`text-text-darker-color ${isDefaultProject ? 'bg-[#d0cec7] w-[100px] h-3 rounded-[5px]' : ''}`} data-default="true"></div>
               <div className="flex justify-between items-center text-text-darker-color" data-default="true">
                 <div className="rounded-[5px] bg-[#d0cec7] w-[76px] h-3" />
@@ -125,7 +127,7 @@ export default function ProjectCard({
             </div>
           ) : (
             project && (
-              <div className="flex flex-col gap-2.5 mt-[30px] text-text-size-small">
+              <div className="flex flex-col mt-6 gap-2.5 text-text-size-small flex-1">
                 <span className="text-text-darker-color">
                   {`${totalBoards} tableau${totalBoards === 1 ? "" : "x"}`}
                 </span>
