790664b2b21063f486caad02598bdcf8c65adb6b
Aurélien Kohler <aurelien@akdigital.fr>
2025-09-22 23:50:50 +0200

    update notification.js

--- client/src/api/notification.js
+++ client/src/api/notification.js
@@ -1,6 +1,6 @@
 "use client";
 
-const API_URL = process.env.API_URL || "http://localhost:5001/api";
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5001/api";
 
 // Fonction pour obtenir le token depuis les cookies
 function getCookieValue(name) {
